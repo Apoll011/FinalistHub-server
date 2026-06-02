c4c7ce8295b25a692ca2bad073fedb77759afb8b
apoll011 <tiagorobotik@gmail.com>
2026-06-02 18:56:55 +0100

    Remove unused volume configuration from docker-compose.yml

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 
 test_mode = os.getenv("TEST", "false").lower() == "true"
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./finalisthub_test.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./db/finalisthub_test.db")
 print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if test_mode else {})
 
